8cf3ed8d00211aa5af9dfe5a9a487fabf73b399e
孙行者网络 <528096212@qq.com>
2018-03-15 11:09:37 +0800

    初始化项目

---
+++ GFCardText/GFCardText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GFCardText
+//
+//  Created by 孙行者网络 on 2018/3/15.
+//  Copyright © 2018年 孙行者网络. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GFCardText/GFCardText/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GFCardText
+//
+//  Created by 孙行者网络 on 2018/3/15.
+//  Copyright © 2018年 孙行者网络. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
